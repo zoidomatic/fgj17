6bce2060a28ee9b94eb8e9c45e7fcb0957b7e66c
Elias <eliasvahis@luukku.com>
2017-01-21 01:06:08 +0200

    korjaus edelliseen

---
+++ WaveBreaker/Assets/Scripts/expander.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class expander : MonoBehaviour {
+
+	public float speed;
+	private float truespeed;
+	// Use this for initialization
+	void Start () {
+		truespeed = speed * .1F;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale += new Vector3 (truespeed, 0, truespeed);
+	}
+
+
+}
---
+++ WaveBreaker/Assets/Scripts/waveBlaster.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waveBlaster : MonoBehaviour {
+
+	private int cooldown;
+	public GameObject wave;
+	public Transform wavespawn;
+	// Use this for initialization
+	void Start () {
+		cooldown = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cooldown > 0)
+			cooldown -= 1;
+		
+		if (Input.GetKey ("up") && cooldown == 0) {
+			wavespawn.position = new Vector3 (5F,0,0);
+			Instantiate (wave, wavespawn.position, wavespawn.rotation);
+			cooldown = 40;
+		}
+		if (Input.GetKey ("down") && cooldown == 0) {
+			wavespawn.position = new Vector3 (-5F,0,0);
+			Instantiate (wave, wavespawn.position, wavespawn.rotation);
+			cooldown = 40;
+		}
+		if (Input.GetKey ("left") && cooldown == 0) {
+			wavespawn.position = new Vector3 (0,0,5F);
+			Instantiate (wave, wavespawn.position, wavespawn.rotation);
+			cooldown = 40;
+		}
+		if (Input.GetKey ("right") && cooldown == 0) {
+			wavespawn.position = new Vector3 (0,0,-5F);
+			Instantiate (wave, wavespawn.position, wavespawn.rotation);
+			cooldown = 40;
+		}
+	}
+}

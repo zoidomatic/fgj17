4fb0c261591071ae869b4625f4e8bbadaad4a8d2
Tyaisurm <jaakko.tuuri@windowslive.com>
2017-01-21 17:06:53 +0200

    edited scoretext

--- WaveBreaker/Assets/Scripts/waveBlaster.cs
+++ WaveBreaker/Assets/Scripts/waveBlaster.cs
@@ -22,7 +22,7 @@ public class waveBlaster : MonoBehaviour {
 	public Transform wavespawn;
 	public GameObject flashscreen;
 	public static Vector3 pokspos;
-    Text scoreText;
+    TextMesh scoreText;
     public AudioClip[] clips;
     private AudioSource[] audioSources;
     // Use this for initialization
@@ -34,7 +34,7 @@ public class waveBlaster : MonoBehaviour {
 		cooldown = 0;
 		aicool = 30;
         score = 0;
-        scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+        scoreText = GameObject.Find("ScoreText").GetComponent<TextMesh>();
         addToScore(0);
 		firerate = 1;
 

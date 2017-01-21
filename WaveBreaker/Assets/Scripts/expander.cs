b22854ae0d68333a4244a48f1b5d0d51f94a820a
Elias <eliasvahis@luukku.com>
2017-01-21 10:08:06 +0200

    game over

--- WaveBreaker/Assets/Scripts/expander.cs
+++ WaveBreaker/Assets/Scripts/expander.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class expander : MonoBehaviour {
 
 	public float speed;
 	private float truespeed;
+	Text scoreText;
 	// Use this for initialization
 	void Start () {
 		truespeed = speed * .1F;
+		scoreText = GameObject.Find("ScoreText").GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +19,7 @@ public class expander : MonoBehaviour {
 		transform.localScale += new Vector3 (truespeed, 0, truespeed);
 
 		if (transform.localScale.x > 25) {
+			scoreText.text = "GAME OVER";
 			Destroy (gameObject);
 		}
 	}

4e110736818a1243a03774457c5cc25db74ee191
zoidomatic <jonna.e.laaksonen@gmail.com>
2017-01-21 09:28:38 +0200

    Score is now added based on the scale of the waves.
    Should have the highest score when scale is 10, the wave is at the center.

--- WaveBreaker/Assets/Scripts/Annihilator.cs
+++ WaveBreaker/Assets/Scripts/Annihilator.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 public class Annihilator : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.transform.position.x + transform.position.x == 0) {
+            // Calculate score based on the collision
+            float multiplier = 10;
+            int score = 0;
+            if(transform.localScale.x > 20 || transform.localScale.x < 0)
+            {
+                score = 0;
+            }
+            else if(transform.localScale.x < 10)
+            {
+                score = (int)(multiplier * (transform.localScale.x));
+            }
+            else
+            {
+                score = (int)(multiplier * (20-(transform.localScale.x)));
+            }
+            
+            Debug.Log("scale x " + transform.localScale.x + "Score: " + score);
+            GameObject.Find("wavespawn").GetComponent<waveBlaster>().addToScore(score);
 			Destroy (other.gameObject);
 			Destroy (gameObject);
 		}
